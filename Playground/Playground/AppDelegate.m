e3d05ced1a55a2f25a5e0a632aa21c39b23e66e2
Tong Kuo <contact@tong-kuo.me>
2015-10-04 18:21:53 +0800

    Introduced SSSearchBar class

--- Playground/Playground/AppDelegate.m
+++ Playground/Playground/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "SearchStuff.h"
 
 // Private Interfaces
 @interface AppDelegate ()
@@ -21,12 +22,31 @@ NSString* const kSearchStuffWidget = @"kSearchStuffWidget";
 
 - ( NSArray* ) toolbarAllowedItemIdentifiers: ( NSToolbar* )_Toolbar
     {
-    return @[ NSToolbarFlexibleSpaceItemIdentifier, NSToolbarShowColorsItemIdentifier, NSToolbarFlexibleSpaceItemIdentifier ];
+    return @[ NSToolbarFlexibleSpaceItemIdentifier, kSearchStuffWidget, NSToolbarFlexibleSpaceItemIdentifier ];
     }
 
 - ( NSArray* ) toolbarDefaultItemIdentifiers: ( NSToolbar* )_Toolbar
     {
-    return @[ NSToolbarFlexibleSpaceItemIdentifier, NSToolbarShowColorsItemIdentifier, NSToolbarFlexibleSpaceItemIdentifier ];
+    return @[ NSToolbarFlexibleSpaceItemIdentifier, kSearchStuffWidget, NSToolbarFlexibleSpaceItemIdentifier ];
+    }
+
+- ( NSToolbarItem* )  toolbar: ( NSToolbar* )_Toolbar
+        itemForItemIdentifier: ( NSString* )_ItemIdentifier
+    willBeInsertedIntoToolbar: ( BOOL )_Flag
+    {
+    NSToolbarItem* toolbarItem = nil;
+
+    if ( [ _ItemIdentifier isEqualToString: kSearchStuffWidget ] )
+        {
+        toolbarItem = [ [ NSToolbarItem alloc ] initWithItemIdentifier: @"Search Stuff" ];
+
+        SSSearchBar* searchBar= [ [ SSSearchBar alloc ] initWithFrame: NSMakeRect( 0, 0, 0, 0 ) ];
+        [ toolbarItem setView: searchBar ];
+        [ toolbarItem setMinSize: NSMakeSize( 100, 24 ) ];
+        [ toolbarItem setMaxSize: NSMakeSize( 1000, 24 ) ];
+        }
+
+    return toolbarItem;
     }
 
 @end // AppDelegate class
---
+++ Playground/SearchStuff/SearchStuff.h
@@ -0,0 +1,14 @@
+//
+//  SearchStuff.h
+//  Playground
+//
+//  Created by Tong G. on 10/4/15.
+//  Copyright © 2015 Tong Kuo. All rights reserved.
+//
+
+#ifndef SearchStuff_h
+#define SearchStuff_h
+
+#import "SSSearchBar.h"
+
+#endif /* SearchStuff_h */
