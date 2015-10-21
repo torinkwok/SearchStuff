1e425650ae83411d06c6a99cb5a5ffb70a95f3c6
Tong Kuo <contact@tong-kuo.me>
2015-10-22 04:55:12 +0800

    Applied new ascii art

--- SearchStuff/internal/__SSWidget+__SSPrivate.h
+++ SearchStuff/internal/__SSWidget+__SSPrivate.h
@@ -1,10 +1,26 @@
-//
-//  __SSWidget+__SSPrivate.h
-//  Playground
-//
-//  Created by Tong G. on 10/21/15.
-//  Copyright © 2015 Tong Kuo. All rights reserved.
-//
+/*=============================================================================┐
+|    _______           _     _              ______               ___    ___    |  
+|   (_______)         | |   (_)            / _____) _           / __)  / __)   |██
+|    _____ _   _  ____| |  _ _ ____   ____( (____ _| |_ _   _ _| |__ _| |__    |██
+|   |  ___) | | |/ ___) |_/ ) |  _ \ / _  |\____ (_   _) | | (_   __|_   __)   |██
+|   | |   | |_| ( (___|  _ (| | | | ( (_| |_____) )| |_| |_| | | |    | |      |██
+|   |_|   |____/ \____)_| \_)_|_| |_|\___ (______/  \__)____/  |_|    |_|      |██
+|                                   (_____|                                    |██
+|          _ ______                        _      ______               ___     |██
+|         | / _____)                      | |    / _____) _           / __)    |██
+|        / ( (____  _____ _____  ____ ____| |__ ( (____ _| |_ _   _ _| |__     |██
+|       / / \____ \| ___ (____ |/ ___) ___)  _ \ \____ (_   _) | | (_   __)    |██
+|      / /  _____) ) ____/ ___ | |  ( (___| | | |_____) )| |_| |_| | | |       |██
+|     |_|  (______/|_____)_____|_|   \____)_| |_(______/  \__)____/  |_|       |██
+|                                                                              |██
+|                                                                              |██
+|                         Copyright (c) 2015 Tong Kuo                          |██
+|                                                                              |██
+|                             ALL RIGHTS RESERVED.                             |██
+|                                                                              |██
+└==============================================================================┘██
+  ████████████████████████████████████████████████████████████████████████████████
+  ██████████████████████████████████████████████████████████████████████████████*/
 
 #import "__SSWidget.h"
 
@@ -12,3 +28,29 @@
 @interface __SSWidget ( __SSPrivate )
 - ( instancetype ) __initWithSSWiget: ( SearchStuffWidget* )_Widget;
 @end // __SSWidget + __SSPrivate
+
+/*===============================================================================┐
+|                                                                                |
+|                           The MIT License (MIT)                                |
+|                                                                                |
+|                        Copyright (c) 2015 Tong Kuo                             |
+|                                                                                |
+| Permission is hereby granted, free of charge, to any person obtaining a copy   |
+| of this software and associated documentation files (the "Software"), to deal  |
+| in the Software without restriction, including without limitation the rights   |
+| to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      |
+|   copies of the Software, and to permit persons to whom the Software is        |
+|         furnished to do so, subject to the following conditions:               |
+|                                                                                |
+| The above copyright notice and this permission notice shall be included in all |
+|              copies or substantial portions of the Software.                   |
+|                                                                                |
+| THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     |
+| IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       |
+| FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    |
+|  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER        |
+| LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  |
+| OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  |
+|                                 SOFTWARE.                                      |
+|                                                                                |
+└===============================================================================*/
