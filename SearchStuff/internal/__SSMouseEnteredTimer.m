795b713f71d880545a399d9c216cc5ea569a73f6
Tong Kuo <contact@tong-kuo.me>
2015-11-05 23:40:26 +0800

    Introduced __SSMouseEnteredTimer to simplify the code

---
+++ SearchStuff/internal/__SSMouseEnteredTimer.m
@@ -0,0 +1,117 @@
+/*=============================================================================┐
+|  _______           _     _              ______               ___    ___      |  
+| (_______)         | |   (_)            / _____) _           / __)  / __)     |██
+|  _____ _   _  ____| |  _ _ ____   ____( (____ _| |_ _   _ _| |__ _| |__ ___  |██
+| |  ___) | | |/ ___) |_/ ) |  _ \ / _  |\____ (_   _) | | (_   __|_   __)___) |██
+| | |   | |_| ( (___|  _ (| | | | ( (_| |_____) )| |_| |_| | | |    | | |___ | |██
+| |_|   |____/ \____)_| \_)_|_| |_|\___ (______/  \__)____/  |_|    |_| (___/  |██
+|                                 (_____|                                      |██
+|                                                                              |██
+|      _ ______                        _      ______               ___    ___  |██
+|     | / _____)                      | |    / _____) _           / __)  / __) |██
+|    / ( (____  _____ _____  ____ ____| |__ ( (____ _| |_ _   _ _| |__ _| |__  |██
+|   / / \____ \| ___ (____ |/ ___) ___)  _ \ \____ (_   _) | | (_   __|_   __) |██
+|  / /  _____) ) ____/ ___ | |  ( (___| | | |_____) )| |_| |_| | | |    | |    |██
+| |_|  (______/|_____)_____|_|   \____)_| |_(______/  \__)____/  |_|    |_|    |██
+|                                                                              |██
+|                                                                              |██
+|                         Copyright (c) 2015 Tong Kuo                          |██
+|                                                                              |██
+|                             ALL RIGHTS RESERVED.                             |██
+|                                                                              |██
+└==============================================================================┘██
+  ████████████████████████████████████████████████████████████████████████████████
+  ██████████████████████████████████████████████████████████████████████████████*/
+
+#import "__SSMouseEnteredTimer.h"
+
+// Private Interfaces
+@interface __SSMouseEnteredTimer ()
+- ( void ) __mouseEnteredTimerFired: ( NSTimer* )_Timer;
+@end // Private Interfaces
+
+// __SSMouseEnteredTimer class
+@implementation __SSMouseEnteredTimer
+    {
+@protected
+    __SSMouseEnteredTimerExcutionBlockType __excutionBlock;
+
+    NSTimeInterval __timeInterval;
+    NSTimer __strong* __backingTimer;
+    }
+
+@dynamic timeInterval;
+
+- ( NSTimeInterval ) timeInterval
+    {
+    return self->__timeInterval;
+    }
+
+#pragma mark - Initializations
+
+- ( instancetype ) initWithTimeInterval: ( NSTimeInterval )_TimeInterval
+                          excutionBlock: ( __SSMouseEnteredTimerExcutionBlockType )_ExcutionBlock
+    {
+    if ( self = [ super init ] )
+        {
+        self->__timeInterval = _TimeInterval;
+        self->__excutionBlock = [ _ExcutionBlock copy ];
+        }
+
+    return self;
+    }
+
+#pragma mark - Starting a Timer
+
+- ( void ) start
+    {
+    self->__backingTimer = [ NSTimer timerWithTimeInterval: self->__timeInterval
+                                                    target: self
+                                                  selector: @selector( __mouseEnteredTimerFired: )
+                                                  userInfo: nil
+                                                   repeats: NO ];
+
+    [ [ NSRunLoop currentRunLoop ] addTimer: self->__backingTimer
+                                    forMode: NSDefaultRunLoopMode ];
+    }
+
+#pragma mark - Stopping a Tiner
+
+- ( void ) stop
+    {
+    [ self->__backingTimer invalidate ];
+    }
+
+#pragma mark - Private Interfaces
+- ( void ) __mouseEnteredTimerFired: ( NSTimer* )_Timer
+    {
+    self->__excutionBlock();
+    }
+
+@end // __SSMouseEnteredTimer class
+
+/*===============================================================================┐
+|                                                                                |
+|                           The MIT License (MIT)                                |
+|                                                                                |
+|                        Copyright (c) 2015 Tong Kuo                             |
+|                                                                                |
+| Permission is hereby granted, free of charge, to any person obtaining a copy   |
+| of this software and associated documentation files (the "Software"), to deal  |
+| in the Software without restriction, including without limitation the rights   |
+| to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      |
+|   copies of the Software, and to permit persons to whom the Software is        |
+|         furnished to do so, subject to the following conditions:               |
+|                                                                                |
+| The above copyright notice and this permission notice shall be included in all |
+|              copies or substantial portions of the Software.                   |
+|                                                                                |
+| THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     |
+| IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       |
+| FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    |
+|  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER        |
+| LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  |
+| OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  |
+|                                 SOFTWARE.                                      |
+|                                                                                |
+└===============================================================================*/
