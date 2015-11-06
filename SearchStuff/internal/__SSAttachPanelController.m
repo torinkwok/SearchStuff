d9967ea818188c906c1fac4d51644431e6d0e665
Tong Kuo <contact@tong-kuo.me>
2015-11-06 19:31:56 +0800

    Updated the ascii art for just introduced sources

--- SearchStuff/internal/__SSAttachPanelController.m
+++ SearchStuff/internal/__SSAttachPanelController.m
@@ -1,17 +1,18 @@
 /*=============================================================================┐
-|             _  _  _       _                                                  |  
-|            (_)(_)(_)     | |                            _                    |██
-|             _  _  _ _____| | ____ ___  ____  _____    _| |_ ___              |██
-|            | || || | ___ | |/ ___) _ \|    \| ___ |  (_   _) _ \             |██
-|            | || || | ____| ( (__| |_| | | | | ____|    | || |_| |            |██
-|             \_____/|_____)\_)____)___/|_|_|_|_____)     \__)___/             |██
+|  _______           _     _              ______               ___    ___      |  
+| (_______)         | |   (_)            / _____) _           / __)  / __)     |██
+|  _____ _   _  ____| |  _ _ ____   ____( (____ _| |_ _   _ _| |__ _| |__ ___  |██
+| |  ___) | | |/ ___) |_/ ) |  _ \ / _  |\____ (_   _) | | (_   __|_   __)___) |██
+| | |   | |_| ( (___|  _ (| | | | ( (_| |_____) )| |_| |_| | | |    | | |___ | |██
+| |_|   |____/ \____)_| \_)_|_| |_|\___ (______/  \__)____/  |_|    |_| (___/  |██
+|                                 (_____|                                      |██
 |                                                                              |██
-|                 ______                   _  _  _ _ _     _ _                 |██
-|                (_____ \                 (_)(_)(_|_) |   (_) |                |██
-|                 _____) )   _  ____ _____ _  _  _ _| |  _ _| |                |██
-|                |  ____/ | | |/ ___) ___ | || || | | |_/ ) |_|                |██
-|                | |    | |_| | |   | ____| || || | |  _ (| |_                 |██
-|                |_|    |____/|_|   |_____)\_____/|_|_| \_)_|_|                |██
+|      _ ______                        _      ______               ___    ___  |██
+|     | / _____)                      | |    / _____) _           / __)  / __) |██
+|    / ( (____  _____ _____  ____ ____| |__ ( (____ _| |_ _   _ _| |__ _| |__  |██
+|   / / \____ \| ___ (____ |/ ___) ___)  _ \ \____ (_   _) | | (_   __|_   __) |██
+|  / /  _____) ) ____/ ___ | |  ( (___| | | |_____) )| |_| |_| | | |    | |    |██
+| |_|  (______/|_____)_____|_|   \____)_| |_(______/  \__)____/  |_|    |_|    |██
 |                                                                              |██
 |                                                                              |██
 |                         Copyright (c) 2015 Tong Kuo                          |██
@@ -19,6 +20,7 @@
 |                             ALL RIGHTS RESERVED.                             |██
 |                                                                              |██
 └==============================================================================┘██
+  ████████████████████████████████████████████████████████████████████████████████
   ██████████████████████████████████████████████████████████████████████████████*/
 
 #import "__SSAttachPanelController.h"
@@ -187,21 +189,27 @@
 @end // __SSAttachPanelController class
 
 /*===============================================================================┐
-|                                                                                | 
-|                      ++++++     =++++~     +++=     =+++                       | 
-|                        +++,       +++      =+        ++                        | 
-|                        =+++       ~+++     +        =+                         | 
-|                         +++=       =++=   +=        +                          | 
-|                          +++        +++= +=        +=                          | 
-|                          =+++        ++++=        =+                           | 
-|                           +++=       =+++         +                            | 
-|                            +++~       +++=       +=                            | 
-|                            ,+++      ~++++=     ==                             | 
-|                             ++++     +  +++     +                              | 
-|                              +++=   +   ~+++   +,                              | 
-|                               +++  +:    =+++ ==                               | 
-|                               =++++=      +++++                                | 
-|                                +++=        +++                                 | 
-|                                 ++          +=                                 | 
-|                                                                                | 
+|                                                                                |
+|                           The MIT License (MIT)                                |
+|                                                                                |
+|                        Copyright (c) 2015 Tong Kuo                             |
+|                                                                                |
+| Permission is hereby granted, free of charge, to any person obtaining a copy   |
+| of this software and associated documentation files (the "Software"), to deal  |
+| in the Software without restriction, including without limitation the rights   |
+| to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      |
+|   copies of the Software, and to permit persons to whom the Software is        |
+|         furnished to do so, subject to the following conditions:               |
+|                                                                                |
+| The above copyright notice and this permission notice shall be included in all |
+|              copies or substantial portions of the Software.                   |
+|                                                                                |
+| THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     |
+| IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       |
+| FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    |
+|  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER        |
+| LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  |
+| OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  |
+|                                 SOFTWARE.                                      |
+|                                                                                |
 └===============================================================================*/
